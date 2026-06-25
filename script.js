56401e5c76d45626500bde0f2fc5415f74671cf0
Vedika <vedikaa89@gmail.com>
2026-06-25 19:50:36 +0530

    Added JavaScript functionality for booking buttons

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const buttons = document.querySelectorAll("button");
 
+buttons.forEach(button => {
+    button.addEventListener("click", function () {
+        this.innerHTML = "Booked";
+        this.style.backgroundColor = "green";
+    });
+});
