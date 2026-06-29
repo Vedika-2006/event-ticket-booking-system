74f52e776cf82c8c048dfb553eaf53c9ef84136c
Vedika <vedikaa89@gmail.com>
2026-06-29 20:33:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-const buttons = document.querySelectorAll("button");
+function bookingSuccess() {
 
-buttons.forEach(button => {
-    button.addEventListener("click", function () {
-        this.innerHTML = "Booked";
-        this.style.backgroundColor = "green";
-    });
-});
+    let bookingId = "EVT" + Math.floor(Math.random() * 100000);
+
+    let rows = ["A", "B", "C", "D", "E", "F"];
+    let seat = rows[Math.floor(Math.random() * rows.length)] + "-" + (Math.floor(Math.random() * 30) + 1);
+
+    alert(
+        "🎉 Booking Successful!\n\n" +
+        "Booking ID: " + bookingId +
+        "\nSeat Number: " + seat +
+        "\n\nEnjoy Your Event! 😊"
+    );
+
+    return false;
+}
