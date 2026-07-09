4160192133fb51dee326f4b8337ce617665e4131
Vedika <vedikaa89@gmail.com>
2026-07-09 19:47:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,21 +36,75 @@ function bookingSuccess() {
     let rows = ["A","B","C","D","E","F"];
     let seat = rows[Math.floor(Math.random()*rows.length)] + "-" + (Math.floor(Math.random()*30)+1);
 
-    alert(
-        "🎉 Booking Successful!\n\n" +
-        "Name: " + name +
-        "\nEvent: " + event +
-        "\nTickets: " + tickets +
-        "\nBooking ID: " + bookingId +
-        "\nSeat Number: " + seat +
-        "\n\nEnjoy Your Event! 😊"
-    );
+   document.getElementById("bookingSummary").style.display = "block";
 
+document.getElementById("sName").textContent = name;
+document.getElementById("sEmail").textContent = email;
+document.getElementById("sEvent").textContent = event;
+document.getElementById("sTickets").textContent = tickets;
+document.getElementById("sBooking").textContent = bookingId;
+document.getElementById("sSeat").textContent = seat;
     return false;
 }
+localStorage.setItem("name",name);
+
+localStorage.setItem("email",email);
+
+localStorage.setItem("event",event);
+
+localStorage.setItem("tickets",tickets);
+
+localStorage.setItem("bookingId",bookingId);
+
+localStorage.setItem("seat",seat);
+
 function selectEvent(eventName){
     document.getElementById("event").value = eventName;
 }
 function toggleDarkMode(){
     document.body.classList.toggle("dark-mode");
+}
+const cards=document.querySelectorAll(".event-card");
+
+window.addEventListener("scroll",()=>{
+
+cards.forEach(card=>{
+
+const top=card.getBoundingClientRect().top;
+
+if(top<window.innerHeight-100){
+
+card.classList.add("show");
+
+}
+
+});
+
+});
+window.onscroll=function(){
+
+let btn=document.getElementById("topBtn");
+
+if(document.documentElement.scrollTop>300){
+
+btn.style.display="block";
+
+}else{
+
+btn.style.display="none";
+
+}
+
+}
+
+function topFunction(){
+
+window.scrollTo({
+
+top:0,
+
+behavior:"smooth"
+
+});
+
 }
