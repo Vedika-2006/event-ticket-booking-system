fab0832f89f8055c13b993a0dcdc7cbe98f25105
Vedika <vedikaa89@gmail.com>
2026-07-09 19:04:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
 function bookingSuccess() {
 
+    let name = document.getElementById("name").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let event = document.getElementById("event").value.trim();
+    let tickets = document.getElementById("tickets").value;
+
+    if(name === ""){
+        alert("❌ Please enter your name.");
+        return false;
+    }
+
+    if(email === ""){
+        alert("❌ Please enter your email.");
+        return false;
+    }
+
+    if(!email.includes("@")){
+        alert("❌ Please enter a valid email.");
+        return false;
+    }
+
+    if(event === ""){
+        alert("❌ Please enter the event name.");
+        return false;
+    }
+
+    if(tickets <= 0){
+        alert("❌ Please enter a valid number of tickets.");
+        return false;
+    }
+
     let bookingId = "EVT" + Math.floor(Math.random() * 100000);
 
-    let rows = ["A", "B", "C", "D", "E", "F"];
-    let seat = rows[Math.floor(Math.random() * rows.length)] + "-" + (Math.floor(Math.random() * 30) + 1);
+    let rows = ["A","B","C","D","E","F"];
+    let seat = rows[Math.floor(Math.random()*rows.length)] + "-" + (Math.floor(Math.random()*30)+1);
 
     alert(
         "🎉 Booking Successful!\n\n" +
-        "Booking ID: " + bookingId +
+        "Name: " + name +
+        "\nEvent: " + event +
+        "\nTickets: " + tickets +
+        "\nBooking ID: " + bookingId +
         "\nSeat Number: " + seat +
         "\n\nEnjoy Your Event! 😊"
     );
